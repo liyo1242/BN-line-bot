4e8a1abe76a70e02fbb53434f9c75e20a40b00ae
liyo1242 <liyo1242@gmail.com>
2018-09-21 18:33:33 +0800

    das

--- src/linebot.js
+++ src/linebot.js
@@ -623,7 +623,7 @@ module.exports = class LineBot {
 
     PUBG(message, status) {
         //return Promise.resolve(0)
-        if ((message.message.text.indexOf('Cubee 安安') != -1) || (message.message.text.indexOf('hi Cubee') != -1) || (message.message.text.indexOf('hi cubee') != -1) || (message.message.text.indexOf('Hi Cubee') != -1) || (message.message.text.indexOf('Hi cubee') != -1)) {
+        if ((message.message.text.indexOf('fuck cubee') != -1) || (message.message.text.indexOf('Cubee 安安') != -1) || (message.message.text.indexOf('hi Cubee') != -1) || (message.message.text.indexOf('hi cubee') != -1) || (message.message.text.indexOf('Hi Cubee') != -1) || (message.message.text.indexOf('Hi cubee') != -1)) {
             const confirm = bnreply.cubee(); // end confirm
             return Promise.resolve(confirm);
         } else if ((message.message.text.indexOf('TaxiGo') != -1)) {
